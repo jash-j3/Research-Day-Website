d0c47c8dce5fb75db28d47cb93eb484d909afb44
akoolarni <arinpk210@gmail.com>
2024-01-05 21:35:08 +0530

    commits

---
+++ app/components/Eve.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Eve() {
+  return (
+    <div className=' flex justify-center mt-12'>
+        Eve
+        <div className='container mx:auto  bg-slate-300 hover:bg-white rounded-xl mt-5'>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Eve
