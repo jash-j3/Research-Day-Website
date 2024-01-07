ff308d91e1220b9dd6d8404324b7ea0f16fb0144
jash-j3 <jash.jhatakia@gmail.com>
2024-01-07 12:40:48 +0530

    frontpage changed infinite loop fixed

--- react-home/src/Navbar.jsx
+++ react-home/src/Navbar.jsx
@@ -8,11 +8,11 @@ function Navbar() {
       <div class="nav-items">
         <img src={logo} alt="iith-logo" className="iithlogo" />
         <div className="navtxt">
-            <div className="itm">Home</div>
-            <div className="itm">About</div>
-            <div className="itm">Programme</div>
-            <div className="itm">Registeration</div>
-            <div className="itm">Events & Schedule</div>
+          <div className="itm">Home</div>
+          <div className="itm">About</div>
+          <div className="itm">Programme</div>
+          <div className="itm">Registeration</div>
+          <div className="itm">Events & Schedule</div>
         </div>
       </div>
     </div>
