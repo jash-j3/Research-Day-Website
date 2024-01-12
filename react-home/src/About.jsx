63741b3f1e3bfb8e156222651d2eb5fcf00db97b
akoolarni <arinpk210@gmail.com>
2024-01-12 17:52:44 +0530

    message additions left

--- react-home/src/About.jsx
+++ react-home/src/About.jsx
@@ -15,7 +15,18 @@ function About() {
         />
         <div className="text-content">
           <h2>Message from Director</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit....</p>
+          <p>  Welcome to our organization! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Quisque non elit auctor, faucibus mauris id, bibendum lectus. Integer quis metus a quam
+    varius fermentum. Sed aliquet ipsum ut quam vestibulum, at vulputate nulla dapibus.
+    Curabitur vel nunc vel justo viverra laoreet ut vel ipsum. Nullam vel mi et urna
+    tincidunt rhoncus. Integer non dolor nec tellus accumsan feugiat. Aenean eu velit
+    euismod, lacinia sem ut, scelerisque quam. Suspendisse potenti. Duis bibendum
+    fringilla ligula, sit amet dapibus libero tincidunt sit amet. Nulla facilisi. Ut
+    aliquam tortor ut tortor tincidunt, in laoreet justo scelerisque. Nulla facilisi.
+    Suspendisse potenti. Sed nec quam ac metus lacinia dictum. Duis ac augue id ligula
+    auctor tincidunt non et ex. Sed in ultrices risus, a auctor enim. Sed malesuada,
+    ipsum vitae varius rhoncus, ex augue sollicitudin sapien, nec hendrerit sem velit eu
+    purus.</p>
         </div>
       </div>
       <div className="message">
@@ -26,7 +37,18 @@ function About() {
         />
         <div className="text-content">
           <h2>Message from Dean</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit....</p>
+          <p>  Welcome to our organization! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Quisque non elit auctor, faucibus mauris id, bibendum lectus. Integer quis metus a quam
+    varius fermentum. Sed aliquet ipsum ut quam vestibulum, at vulputate nulla dapibus.
+    Curabitur vel nunc vel justo viverra laoreet ut vel ipsum. Nullam vel mi et urna
+    tincidunt rhoncus. Integer non dolor nec tellus accumsan feugiat. Aenean eu velit
+    euismod, lacinia sem ut, scelerisque quam. Suspendisse potenti. Duis bibendum
+    fringilla ligula, sit amet dapibus libero tincidunt sit amet. Nulla facilisi. Ut
+    aliquam tortor ut tortor tincidunt, in laoreet justo scelerisque. Nulla facilisi.
+    Suspendisse potenti. Sed nec quam ac metus lacinia dictum. Duis ac augue id ligula
+    auctor tincidunt non et ex. Sed in ultrices risus, a auctor enim. Sed malesuada,
+    ipsum vitae varius rhoncus, ex augue sollicitudin sapien, nec hendrerit sem velit eu
+    purus.</p>
         </div>
       </div>
       <div className="message">
@@ -37,7 +59,18 @@ function About() {
         />
         <div className="text-content">
           <h2>Message from OC</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit....</p>
+          <p>  Welcome to our organization! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Quisque non elit auctor, faucibus mauris id, bibendum lectus. Integer quis metus a quam
+    varius fermentum. Sed aliquet ipsum ut quam vestibulum, at vulputate nulla dapibus.
+    Curabitur vel nunc vel justo viverra laoreet ut vel ipsum. Nullam vel mi et urna
+    tincidunt rhoncus. Integer non dolor nec tellus accumsan feugiat. Aenean eu velit
+    euismod, lacinia sem ut, scelerisque quam. Suspendisse potenti. Duis bibendum
+    fringilla ligula, sit amet dapibus libero tincidunt sit amet. Nulla facilisi. Ut
+    aliquam tortor ut tortor tincidunt, in laoreet justo scelerisque. Nulla facilisi.
+    Suspendisse potenti. Sed nec quam ac metus lacinia dictum. Duis ac augue id ligula
+    auctor tincidunt non et ex. Sed in ultrices risus, a auctor enim. Sed malesuada,
+    ipsum vitae varius rhoncus, ex augue sollicitudin sapien, nec hendrerit sem velit eu
+    purus..</p>
         </div>
       </div>
     </div>
--- react-home/src/Sponsorship.jsx
+++ react-home/src/Sponsorship.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import "./Sponsorship.css";
 
-function Sponsorship(){
+function Sponsorship() {
+  const sponsorshipSlabs = [
+    { type: "diamond", title: "Diamond Sponsor", description: "Diamond sponsors " },
+    { type: "platinum", title: "Platinum Sponsor", description: "Platinum sponsors receive exclusive perks..." },
+    { type: "gold", title: "Gold Sponsor", description: "Gold sponsors get valuable exposure..." },
+    { type: "silver", title: "Silver Sponsor", description: "Silver sponsors receive exclusive perks..." },
+    { type: "bronze", title: "Bronze Sponsor", description: "Bronze sponsors get valuable exposure..." },
+  ];
+
   return (
     <div className="sponsorship-container">
       <h2>Sponsorship Details</h2>
 
-      <div className="sponsorship-slab gold">
-        <h3>Gold Slab</h3>
-        <p>Gold sponsors enjoy premium benefits...</p>
-        
-      </div>
-
-      <div className="sponsorship-slab silver">
-        <h3>Silver Slab</h3>
-        <p>Silver sponsors receive exclusive perks...</p>
-     
-      </div>
-
-      <div className="sponsorship-slab bronze">
-        <h3>Bronze Slab</h3>
-        <p>Bronze sponsors get valuable exposure...</p>
- 
-      </div>
+      {sponsorshipSlabs.map((slab) => (
+        <div key={slab.type} className={`sponsorship-slab ${slab.type}`}>
+          <h3>{slab.title}</h3>
+          <p>{slab.description}</p>
+        </div>
+      ))}
 
       <div>
-        <p>
-            For more info, refer:
-        </p>
+        <p>For more info, refer:</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sponsorship
+export default Sponsorship;
