475a662b4d3e32823c63f375b1e401346fdc801f
jash-j3 <jash.jhatakia@gmail.com>
2024-01-12 21:43:23 +0530

    changes

---
+++ react-home/src/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div>
+      <div className="abt-main">About</div>
+      <div className="abt-full">
+        <div className="abt-itm">
+          <div className="abt-item-txt">MESSAGE FROM DIRECTOR</div>
+          <div className="abt-img-txt">
+            <img src="https://picsum.photos/200" alt="img-about" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus, dui a venenatis consectetur, turpis dui vestibulum neque, ac euismod nulla risus vel leo. Mauris vitae libero maximus massa consectetur posuere. Donec maximus turpis ut venenatis vehicula. Vivamus sit amet felis nisl. Mauris laoreet, erat sed facilisis consequat, augue est sagittis purus, id varius nisl odio sit amet eros. </p>
+          </div>
+        </div>
+        <div className="abt-itm">
+          <div className="abt-item-txt">MESSAGE FROM DIRECTOR</div>
+          <div className="abt-img-txt">
+            <img src="https://picsum.photos/200" alt="img-about" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus, dui a venenatis consectetur, turpis dui vestibulum neque, ac euismod nulla risus vel leo. Mauris vitae libero maximus massa consectetur posuere. Donec maximus turpis ut venenatis vehicula. Vivamus sit amet felis nisl. Mauris laoreet, erat sed facilisis consequat, augue est sagittis purus, id varius nisl odio sit amet eros. </p>
+          </div>
+        </div>
+        <div className="abt-itm">
+          <div className="abt-item-txt">MESSAGE FROM DIRECTOR</div>
+          <div className="abt-img-txt">
+            <img src="https://picsum.photos/200" alt="img-about" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus, dui a venenatis consectetur, turpis dui vestibulum neque, ac euismod nulla risus vel leo. Mauris vitae libero maximus massa consectetur posuere. Donec maximus turpis ut venenatis vehicula. Vivamus sit amet felis nisl. Mauris laoreet, erat sed facilisis consequat, augue est sagittis purus, id varius nisl odio sit amet eros. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ react-home/src/Programme.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Programme.css";
+
+function Programme() {
+  return (
+    <div className="program">
+      <p>Programme</p>
+      <div class="timeline">
+  <div class="container left">
+    <div class="content">
+      <h2>Expert Talk</h2>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content">
+      <h2>Seminars</h2>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default Programme;
