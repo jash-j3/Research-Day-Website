793b6423f99657a64fd6eb4d45c34daeffe722be
jash-j3 <jash.jhatakia@gmail.com>
2024-01-24 14:09:40 +0530

    added navbar responsicveness

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,30 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import './Navbar.css';
 //const logo = require("../src/assets/logoiith.png");
+const icon = require("../src/assets/icon.png")
 const logo = require("../src/assets/khoj.png");
 
 function Navbar() {
-  return (
-    <div>
-      <div className="navbar">
-        <div class="nav-items">
-          <img src={logo} alt="iith-logo" className="iithlogo" />
-          <div className="navtxt">
-            <Link to="/Research-Day-Website" className="itm">Home</Link>
-            <Link to="/Research-Day-Website/Aboutnav" className="itm">Events</Link>
-            <Link to="/Research-Day-Website/Schedule" className="itm">Schedule</Link>
-            <Link to="/Research-Day-Website/Registration" className="itm">Registration</Link>
-            <Link to="/Research-Day-Website/Sponsorship" className="itm">Sponsorship</Link>
-          </div>
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+    const closeMenu = () => {
+        setIsNavExpanded(false);
+    };
+
+    return (
+        <div>
+            <nav className="navbar">
+                <img src={logo} alt="iith-logo" className="iithlogo" />
+                <div className={`navtxt ${isNavExpanded ? "expanded" : ""}`}>
+                    <Link to="/Research-Day-Website" className="itm" onClick={closeMenu}>Home</Link>
+                    <Link to="/Research-Day-Website/Aboutnav" className="itm" onClick={closeMenu}>Events</Link>
+                    <Link to="/Research-Day-Website/Schedule" className="itm" onClick={closeMenu}>Schedule</Link>
+                    <Link to="/Research-Day-Website/Registration" className="itm" onClick={closeMenu}>Registration</Link>
+                    <Link to="/Research-Day-Website/Sponsorship" className="itm" onClick={closeMenu}>Sponsorship</Link>
+                </div>
+                <button className="hamburger" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+                    <img src={icon} className="icon" alt="hamburger-menu" />
+                </button>
+            </nav>
+            <main>
+                <Outlet />
+            </main>
         </div>
-      </div>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  );
+    );
 }
 
 export default Navbar;
