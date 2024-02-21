f6aebbe3b5cbc0b71b0969f735f4ad700d74ec78
Ksupekar13 <gsth20r09@sgsitsindore,in>
2024-02-21 22:12:43 +0530

    website

--- src/Expert.jsx
+++ src/Expert.jsx
@@ -5,6 +5,7 @@ import speaker1a from "./assets/logoiith.png";
 import speaker2a from "./assets/Faculty-Prof Vinayak Eswaran.jpeg" ;
 import speaker3a from "./assets/Sastry.jpg";
 import speaker4a from "./assets/kiran_kumar_kuchi.jpg";
+import speaker5a from "./assets/R_sai_chandra.jpeg";
 
 function Expert() {
   return (
@@ -14,12 +15,12 @@ function Expert() {
         <div className="evegrid">
           <div className="d1 discussion">
             <h1>1.Expert Talk 1 </h1>
-            <h1>Date:28th-February-2024</h1>
-            <h1>Time:3:15 - 3:45 pm </h1>
             <div className="discussionmain">
               <img src={speaker2a} alt="speaker1a" className="panelspeaker" />
               <h4 className="subhead" id="a">
               Prof. Vinayak Eswaran
+              <h1>Date:28th-February-2024</h1>
+              <h1>Time:3:15 - 3:45 pm </h1>
               </h4>
               <div className="kk">
                 <p>Prof. IIT Hyderabad</p>
@@ -28,12 +29,12 @@ function Expert() {
           </div>
           <div className="d3 discussion">
             <h1>2. Expert Talk 2</h1>
-            <h1>Date : 29th-February-2024</h1>
-            <h1>Time:4:15 - 4:45 pm </h1>
             <div className="discussionmain">
               <img src={speaker3a} alt="speaker1a" className="panelspeaker" />
               <h4 className="subhead" id="a">
                 Prof. Garikapati Narahari Sastry 
+                <h1>Date : 29th-February-2024</h1>
+                <h1>Time:4:15 - 4:45 pm </h1>
               </h4>
               <div className="kk">
                 <p>Prof. IIT Hyderabad</p>
@@ -57,12 +58,12 @@ function Expert() {
           <div className="d3 discussion">
             <h1>Journey Of Enterprenureship</h1>
             <div className="discussionmain">
-              <img src={speaker1a} alt="speaker1a" className="panelspeaker" />
+              <img src={speaker5a} alt="speaker1a" className="panelspeaker" />
               <h4 className="subhead" id="a">
-                [Speaker Name]
+                Dr. R Sai Chandra
               </h4>
               <div className="kk">
-                <p>Coming Soon...</p>
+                <p>COO Green PMU SEMI Private Ltd.</p>
               </div>
             </div>
           </div>
